fbcad4386e567e32ea0d9e78db0663bc543908c7
mai <mailinh.akemi@gmail.com>
2023-10-24 18:16:22 +0200

    createcell + structure

--- set.c
+++ set.c
@@ -1,3 +1,12 @@
-//
-// Created by mai on 24/10/2023.
-//
+#include "set.h"
+#include <stdlib.h>
+
+//create a cell with parameters a value and the number of pointers
+t_d_cell* CreateCell(int val,int MaxLevelPointer){
+    t_d_cell *newCell = (t_d_cell*)malloc(sizeof(t_d_cell));
+    newCell->value = val;
+    for (int i=0;i<=MaxLevelPointer;i++){
+        newCell->next[i]=NULL;
+    }
+    return newCell;
+}
--- set.h
+++ set.h
@@ -1,8 +1,18 @@
-//
-// Created by mai on 24/10/2023.
-//
-
 #ifndef TEST_SET_H
 #define TEST_SET_H
+
+typedef struct s_d_cell
+{
+    int value;
+    struct s_d_cell **next;
+} t_d_cell;
+
+typedef struct s_d_list
+{
+    t_d_cell **head;
+    int MaxLevelHead;
+} t_d_list;
+
+t_d_cell* CreateCell(int val,int MaxLevelPointer);
 
 #endif //TEST_SET_H
