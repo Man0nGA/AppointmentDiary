3d2d291ccb1f51f36c9ba7c7269d4dddc705400c
mai <mailinh.akemi@gmail.com>
2023-10-31 13:22:18 +0100

    corriger la fct CreateCell

--- set.c
+++ set.c
@@ -5,6 +5,7 @@
 t_d_cell* CreateCell(int val,int MaxLevelPointer){
     t_d_cell *newCell = (t_d_cell*)malloc(sizeof(t_d_cell));
     newCell->value = val;
+    newCell->next = (t_d_cell**) malloc(sizeof(t_d_cell*)*MaxLevelPointer);
     for (int i=0;i<=MaxLevelPointer;i++){
         newCell->next[i]=NULL;
     }
