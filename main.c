14a3477bc2cd3949a66e390d49efb6400961c029
Manon <manon.gautier1@orange.fr>
2023-11-14 17:37:13 +0100

    test CreateCell()

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include "set.h"
 
 int main() {
     printf("Hello, World!\n");
-    printf("This is wonderful !");
-    printf("test");
+    printf("This is wonderful !\n");
+    printf("test\n");
+    t_d_cell* c = CreateCell(5, 2);
+    printf("%d %p %p %p\n", c->value, c->next[0], c->next[1], c->next[2]);
     return 0;
 }
